e081a0b5d29811046eb1d45b79985d18e968cb27
TalitaVi-gif <talitavieira1009@gmail.com>
2025-04-17 00:03:51 -0300

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,6 +1,9 @@
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-    { pageLanguage: 'pt', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
-    'google_translate_element'
-  );
-}
+<script type="text/javascript">
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+      { pageLanguage: 'pt', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
+      'google_translate_element'
+    );
+  }
+</script>
+<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
